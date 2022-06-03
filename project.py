3a4eac0381ed865891003f7ee5b8f16ca0587379
weene4 <106667576+weene4@users.noreply.github.com>
2022-06-03 14:31:24 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,36 @@
+
+name=input('Name:')
+age=input('Age:')
+place_of_birth=input('Place Of Birth:')
+nationality=input('Nationality:')
+state_of_origin=input('State Of Orgin:')
+place_of_residence=input('Place Of Residence:')
+complexion=input('Complexion:')
+weight=input('Weight:')
+height=input('Height:')
+bodytype=input('Bodytype:')
+school_name=input('Name Of Your School:')
+class_level=input('Class:')
+best_subject=input('Best Subject:')
+best_meal=input('Best Meal:')
+hobby=input('Hobby:')
+destination=input('Dream Vacaton Destination:')
+dream_job=input('Dream Job:')
+madlib=(
+        f"My name is {name}. I am {age} years old , I was born in {place_of_birth} and I am a {nationality}."
+        f"\nI hail from {state_of_origin} and I live in {place_of_residence}. I am {complexion} in complexion and "
+        f" I must say it is definitely a good color on me. \nI weigh {weight}"
+        f"and also have a rather lovely height of {height}. As you can imagine, these two of my body\'s qualities"
+        f"gives me a {bodytype} bodytype. Don\'t I look amazing already! \n\tI love education so I am definitely"
+        f"going to talk about my school. I attend {school_name} and it is a really lovely school. I am in the"
+        f" {class_level} class. My classmates are al kinds of funny and weird at the same time. You probably would"
+        f"love to know my best subject right? I Love {best_subject}, who would not? It\'s really fun to learn and "
+        f"I totally enjoy it. \n\tThis write up will not be complete if I fail to mentioned my favorite delicacy."
+        f" My best meal is {best_meal} and my Mom is a goddess when it comes to preparing it,she made me love it."
+        f"As you would also imagine,I got hobbies too. In my free time,I love {hobby}. It is always relaxing for me."
+        f"I am grown,yeah,so danm right I think about vacations and my favorite destination would be {destination},"
+        f" we both know it\'s a lovely place. I would love to be {dream_job} in the Technology world, and thanks to "
+        f" CHINGU.IO , I am on my path to achieving that dream. \n\tThat is me and I hope after reading this little "
+        f"you now know me a bit better."
+)
+print(madlib)
